4f226937452a8d7630172e4bbc1eeabe414f48a0
Salihan Mridha <salihanmridha@gmail.com>
2024-01-22 10:35:11 +0600

    migration and model created

---
+++ src/database/migrations/2024_01_22_042328_create_generated_images_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('generated_images', function (Blueprint $table) {
+            $table->id();
+            $table->string("keyword", 255);
+            $table->enum("status", ["PROCESSING", "COMPLETED", "FAILED"])->default("PROCESSING");
+            $table->text("prompt")->nullable();
+            $table->string('file_name', 255)->nullable();
+            $table->string('src', 255)->nullable();
+            $table->text('response')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('generated_images');
+    }
+};
